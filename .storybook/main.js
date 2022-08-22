9e453c3b1230ea7842497772c6ec3d11a5bddc51
JunYong <jypark3@yookgak.com>
2022-08-22 16:05:25 +0900

    change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 const tsconfigPaths = require('vite-tsconfig-paths').default
 // const { mergeConfig } = require('vite')
 const path = require('path')
-const svgrPlugin = require('vite-plugin-svgr').default
+const svgrPlugin = require('vite-plugin-svgr')
+const { loadEnv } = require('vite')
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -12,18 +13,33 @@ module.exports = {
     '@storybook/addon-interactions',
     // '@storybook/addon-actions',
   ],
-  // webpackFinal: async (config) => ({
-  //   ...config,
-  //   resolve: {
-  //     ...config.resolve,
-  //     alias: {
-  //       ...config.resolve.alias,
-  //       // '@': path.resolve(__dirname, '../src/'),
-  //       // '@emotion/core': toPath('node_modules/@emotion/react'),
-  //     },
-  //   },
-  // }),
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        // '@': path.resolve(__dirname, '../src/'),
+        // '@emotion/core': toPath('node_modules/@emotion/react'),
+      },
+    },
+  }),
   viteFinal: async (config, { configType }) => {
+    const env = loadEnv(config.mode, process.cwd())
+    if (process.env.NODE_ENV === 'development') {
+      config.server.host = true
+      config.server.proxy = {
+        '/iam': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        '/admin': {
+          target: env.VITE_CS_URL,
+          changeOrigin: true,
+        },
+      }
+    }
+
     config.resolve.alias = [
       {
         find: '@',
