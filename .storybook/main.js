efb66878e7ccec7114a0df48898646bdf872da6e
JunYong <jypark3@yookgak.com>
2022-11-10 09:44:12 +0900

    메모리 릭 테스트 - 12

--- .storybook/main.js
+++ .storybook/main.js
@@ -28,7 +28,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    // '@storybook/addon-interactions',
+    '@storybook/addon-interactions',
     // '@storybook/addon-controls',
     // '@storybook/addon-links',
     // '@storybook/addon-essentials',
